b3333c0d38b1ca3d4083d1d26d75f8eea12f816f
서주홍 <wnghdsla00@gmail.com>
2026-02-09 16:33:38 +0900

    feat: week1 day2 variables  and operators

--- java-week1/java-week1/src/Main1.java
+++ java-week1/java-week1/src/Main1.java
@@ -1,6 +1,10 @@
 public class Main1 {
-    public static void main() {
-        System.out.println("Hello Java");
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println("두 수의 합: " + (a + b));
+        System.out.println("두 수의 곱: " + (a * b));
 
     }
 }
