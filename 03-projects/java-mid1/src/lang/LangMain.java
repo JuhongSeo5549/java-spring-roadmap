e945b46845913970e8138e322af79cf775de77b4
서주홍 <wnghdsla00@gmail.com>
2026-03-03 17:47:38 +0900

    20260303

---
+++ 03-projects/java-mid1/src/lang/LangMain.java
@@ -0,0 +1,13 @@
+package lang;
+//java.lang 패키지는 모든 자바 애플리케이션에 자동으로 임포트(import)된다. 임포트 구문을 사용하지 않아도 된다.
+//import java.lang.System;
+
+public class LangMain {
+
+    static void main(String[] args) {
+        //java.lang 패키지는 모든 자바 애플리케이션에 자동으로 임포트(import)된다. 임포트 구문을 사용하지 않아도 된다.
+        //ㄴㅁㅇ
+        System.out.println("hello java");
+
+    }
+}
